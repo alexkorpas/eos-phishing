403f516d6c9495eef28bb68563f932e6ad6ddcd5
fistaco <fiske_schijlen@hotmail.com>
2019-09-16 11:14:25 +0200

    Load the full DF and plot top 10 occurrence counts

---
+++ process_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import glob
+
+# Load the data frame
+csv_files = glob.glob("./data/*.csv")
+dfs = [pd.read_csv(path, sep='","', engine="python") for path in csv_files]
+# df = pd.read_csv("./data/all_phishing_from_1.csv", sep='","', engine="python")
+df = pd.concat(dfs, axis=0)
+del dfs  # Garbage collection
+
+# Compute occurrence counts
+for attribute in ["ip", "country", "domain", "email"]:
+    occurrence_counts = df[attribute].value_counts().head(10)
+    
+    # Plot the top 10 occurrence counts
+    occurrence_counts.plot.bar()
+    plt.tight_layout()
+    plt.show()
+
+# Compute and plot the phishing sources with the longest uptime
+
