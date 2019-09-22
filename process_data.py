138aa9ef5741f1732c6d9fad46c4cbf66f7acd4e
Alexandros Korpas Kamarianos <kkalexandros@gmail.com>
2019-09-22 20:14:37 +0200

    Uptime over time

--- process_data.py
+++ process_data.py
@@ -1,3 +1,4 @@
+#%%
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -5,19 +6,16 @@ import glob
 
 from collections import defaultdict, OrderedDict
 
-# Load the data frame
-# csv_files = glob.glob("./data/*.csv")
-# dfs = [pd.read_csv(path, sep='","', engine="python") for path in csv_files]
-# # df = pd.read_csv("./data/all_phishing_from_1.csv", sep='","', engine="python")
-# df = pd.concat(dfs, axis=0)
-# del dfs  # Garbage collection
+# %% Load the data frame
+csv_files = glob.glob("./data/*.csv")
+dfs = [pd.read_csv(path, sep='","', engine="python") for path in csv_files]
+df = pd.concat(dfs, axis=0)
+del dfs  # Garbage collection
+# df = pd.read_csv("./data/all_phishing_from_1.csv", sep='","', engine="python")
 
-df = pd.read_csv("./data/all_phishing_from_1.csv", sep='","', engine="python")
-
-# Compute occurrence counts
+#%% Compute occurrence counts
 for attribute in ["ip", "country", "domain", "email"]:
     occurrence_counts = df[attribute].value_counts().head(10)
-    
     # Plot the top 10 occurrence counts
     occurrence_counts.plot.bar()
     plt.title(f"Top 10 {attribute} occurrence counts")
@@ -26,62 +24,28 @@ for attribute in ["ip", "country", "domain", "email"]:
     # plt.show()
     plt.clf()
 
-# # Compute and plot phishing source uptime
-# uptimes = pd.DataFrame(np.zeros((len(df), 2)), columns=["ip", "uptime"])
-
+#%% Compute average uptime over time
 # Convert all dates to datetime objects and add them to the DF
+# by fixing firsttime and lasttime
+max_dt = df['lasttime'].max()
+df = df[df['firsttime'] != '1970-01-01 01:00:00']
+df.loc[df['lasttime'] < df['firsttime']] = max_dt
 df["start_dt"] = pd.to_datetime(df["firsttime"])
 df["end_dt"] = pd.to_datetime(df["lasttime"])
 
-# Unknown lasttimes are indicated as 01-01-1970. We convert these to the date
-# on which the data set was last updated.
-final_date = max(df.end_dt)
-for (index, row) in df.iterrows():
-    if row.end_dt.year == 1970:
-        df.loc[index, "end_dt"] = final_date
+# Calculate the uptime for each entry, group them by month and find average
+df1 = df[["start_dt", "end_dt"]]
+df1["uptime"] = (df1["end_dt"] - df1["start_dt"]).astype('timedelta64[s]')
+df1 = df1.drop("end_dt", axis=1)
+df2 = df1.groupby(df1["start_dt"].dt.to_period("M")).mean()
 
-# # Determine whether a given phishing source was active in a given month by giv
-
-# Compute the proportion of unique phishing sources per month
-# unique_ips = df.ip.unique()
-
-# Compute the average phishing source uptime per month
-earliest_year = min(df.start_dt).year
-month_uptimes = defaultdict(int)
-month_row_amnts = defaultdict(int)
-for (_, row) in df.iterrows():
-    year = row.start_dt.year
-    month = row.start_dt.month
-    
-    # Ignore rows with an unknown start date
-    if year == 1970:
-        continue
-
-    uptime = (row.end_dt - row.start_dt).days
-
-    idx = (year - 2000)*12 + month
-
-    month_uptimes[idx] += uptime
-    month_row_amnts[idx] += 1
-
-# Normalise each month's uptime
-normalised_month_uptimes = month_uptimes.copy()
-for idx in month_uptimes.keys():
-    if month_row_amnts[idx] == 0:
-        continue
-    normalised_month_uptimes[idx] = month_uptimes[idx]/month_row_amnts[idx]
-
-# Plot the average uptime per month
-ordered_pairs = OrderedDict(sorted(normalised_month_uptimes.items()))
-keys = [pair[0] for pair in ordered_pairs]
-values = [pair[1] for pair in ordered_pairs]
-plt.bar(keys, values)
+# Plot the results
+df2.plot()
 plt.title(f"Average uptime over time")
-plt.ylabel("Days")
-plt.xlabel("Month")
-# plt.tight_layout()
-# plt.savefig(f"./fig/{attribute}-occurrence-counts.png")
-plt.show()
+plt.ylabel("Uptime (seconds)")
+plt.xlabel("Date")
+plt.tight_layout()
+plt.savefig("fig/uptime-vs-date.png")
+plt.clf()
 
-# Compute the percentage of phishing attcks hosted on HTTPS
-
+#%% Compute the percentage of phishing attacks hosted on HTTPS per year
