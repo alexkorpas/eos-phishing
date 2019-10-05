001689ba3019b281f8bd6420ad3c0383c16921e8
Alexandros Korpas Kamarianos <kkalexandros@gmail.com>
2019-10-05 17:20:42 +0200

    Calculate and plot ROSI

--- block3/rosi.py
+++ block3/rosi.py
@@ -25,12 +25,41 @@ df2 = df2.drop(2017)
 
 # Assume that future year will be year will be increased by 500K with a
 # deviation of 10% and plot annual frequency
-mu = df2.max() + 500000
-variance = mu * 0.1
-values = np.random.normal(mu, variance, 1000000)
-plt.hist(values, 50, weights=np.ones(len(values)) / len(values))
+annual_freq_mean = (df2.max() + 500000) * 0.167
+annual_freq_std = annual_freq_mean * 0.1
+annual_freq = np.random.normal(annual_freq_mean, annual_freq_std, 1000000)
+plt.hist(annual_freq, 50, weights=np.ones(len(annual_freq)) / len(annual_freq))
 plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
 plt.savefig('fig/annual_freq.png')
 plt.clf()
 
 
+#%% Distribution of the Unitary impact
+unit_impact_mean = 900
+unit_impact_std = unit_impact_mean * 0.1
+unit_impact = np.random.normal(unit_impact_mean, unit_impact_std, 1000000)
+plt.hist(unit_impact, 50, weights=np.ones(len(unit_impact)) / len(unit_impact))
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.savefig('fig/unit_impact.png')
+plt.clf()
+
+#%% Distribution of the Risk exposure
+exposure_mean = 327750840
+exposure_std = exposure_mean * 0.1
+exposure = np.random.normal(exposure_mean, exposure_std, 1000000)
+plt.hist(exposure, 50, weights=np.ones(len(exposure)) / len(exposure))
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.xlabel('Risk Exposure (e+8 dollars)')
+plt.ylabel('Probability')
+plt.savefig('fig/exposure.png')
+plt.clf()
+
+#%% Calculate ROSI
+cost = 256000
+rosi = (exposure * 0.575 - cost) / cost
+plt.hist(rosi, 50, weights=np.ones(len(rosi)) / len(rosi))
+plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.xlabel('ROSI')
+plt.ylabel('Probability')
+plt.savefig('fig/rosi.png')
+plt.clf()
