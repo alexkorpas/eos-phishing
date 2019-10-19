6cb67ef79d8562e38bdae638407fe81371288323
fistaco <fiske_schijlen@hotmail.com>
2019-10-19 11:19:18 +0200

    Correctly get PCC heatmap

--- block4/compute_factor_influence.py
+++ block4/compute_factor_influence.py
@@ -5,22 +5,28 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns
 from matplotlib.ticker import PercentFormatter
 from scipy.stats import norm
 
 #%% Load the data frame
 
-# csv_files = glob.glob("../data/*.csv")
-# dfs = [pd.read_csv(path, sep='","', engine="python") for path in csv_files]
-# df = pd.concat(dfs, axis=0)
-# del dfs  # Garbage collection
-df = pd.read_csv("./../data/all_phishing_from_1.csv", sep='","', engine="python")
+csv_files = glob.glob("../data/*.csv")
+dfs = [pd.read_csv(path, sep='","', engine="python") for path in csv_files]
+df = pd.concat(dfs, axis=0)
+del dfs  # Garbage collection
+# df = pd.read_csv("./../data/all_phishing_from_1.csv", sep='","', engine="python")
 
 #%% Compute average uptime over time
 # Convert all dates to datetime objects and add them to the DF
 # by fixing firsttime and lasttime
 max_dt = df['lasttime'].max()
 df = df[df['firsttime'] != '1970-01-01 01:00:00']
+
+# Add column "event": 0 if still up, otherwise 1. The "event" is the end of the
+# phishing source's uptime.
+# df["event"] = 
+
 df.loc[df['lasttime'] < df['firsttime'], 'lasttime'] = max_dt
 df["start_dt"] = pd.to_datetime(df["firsttime"])
 df["end_dt"] = pd.to_datetime(df["lasttime"])
@@ -41,7 +47,7 @@ df = df[['uptime', 'country']]
 
 #%% Merge our processed df with the country IP blacklist data
 # Load the CSV that maps countries to amounts of blacklisted IPs
-bl_df = pd.read_csv("./../data/country-ip-blacklists.csv", sep=",")
+bl_df = pd.read_csv("./../data/blacklist/country-ip-blacklists.csv", sep=",")
 bl_df = bl_df[['Country', 'Listings']]  # Only keep relevant columns
 bl_df = bl_df.rename(columns={'Country': 'country', 'Listings': 'listings'})
 
@@ -50,6 +56,12 @@ merged_df = pd.merge(df, bl_df, on="country")
 merged_df = merged_df[merged_df.listings.notnull()]  # Cull NaN-containing rows
 
 # Calculate correlation matrix for the numerical values
-analysis_df = merged_df[['uptime', 'listings']]
+analysis_df = merged_df[['uptime', 'listings', 'event']]
 corr_mat = analysis_df.corr()
 print(corr_mat)
+
+# Create a heatmap for the results
+sns.heatmap(corr_mat, annot=True)  # Requires matplotlib version <=3.1.0
+plt.tight_layout()
+plt.show()
+# plt.savefig("./fig/pearson-corr-heatmap.png")
