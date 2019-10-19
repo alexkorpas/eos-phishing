09f9f640e9e5ee080a9731b020f643e8badf91c1
fistaco <fiske_schijlen@hotmail.com>
2019-10-19 12:19:48 +0200

    Process normalised data

--- block4/compute_factor_influence.py
+++ block4/compute_factor_influence.py
@@ -24,8 +24,9 @@ max_dt = df['lasttime'].max()
 df = df[df['firsttime'] != '1970-01-01 01:00:00']
 
 # Add column "event": 0 if still up, otherwise 1. The "event" is the end of the
-# phishing source's uptime.
-# df["event"] = 
+# phishing source's uptime. A source in the dataset is still up if the
+# last-seen date is specified to be in 1970.
+df["event"] = (df["lasttime"] != '1970-01-01 01:00:00').astype(int)
 
 df.loc[df['lasttime'] < df['firsttime'], 'lasttime'] = max_dt
 df["start_dt"] = pd.to_datetime(df["firsttime"])
@@ -43,20 +44,26 @@ final_date = max(df.end_dt)
 df["uptime"] = (df["end_dt"] - df["start_dt"]).astype('timedelta64[s]')
 
 # Drop columns that we won't be using for statistical analysis
-df = df[['uptime', 'country']]
+df = df[['uptime', 'country', 'event']]
 
 #%% Merge our processed df with the country IP blacklist data
 # Load the CSV that maps countries to amounts of blacklisted IPs
 bl_df = pd.read_csv("./../data/blacklist/country-ip-blacklists.csv", sep=",")
-bl_df = bl_df[['Country', 'Listings']]  # Only keep relevant columns
-bl_df = bl_df.rename(columns={'Country': 'country', 'Listings': 'listings'})
+
+# Format the IPop% column, which is the normalised version of "listings"
+bl_df = bl_df[(bl_df["IPop%"] != "n/a%") & (bl_df["IPop%"] != "\xa0")]
+bl_df["listings_norm"] = bl_df["IPop%"].apply(lambda s: s.strip("%")).astype(float)
+
+# Only keep relevant columns for the analysis
+bl_df = bl_df[['Country', 'listings_norm']]
+bl_df = bl_df.rename(columns={'Country': 'country'})
 
 # Merge our phishing attempt df and country blacklist df
 merged_df = pd.merge(df, bl_df, on="country")
-merged_df = merged_df[merged_df.listings.notnull()]  # Cull NaN-containing rows
+merged_df = merged_df[merged_df.listings_norm.notnull()]  # Cull NaN-containing rows
 
 # Calculate correlation matrix for the numerical values
-analysis_df = merged_df[['uptime', 'listings', 'event']]
+analysis_df = merged_df[['uptime', 'listings_norm', 'event']]
 corr_mat = analysis_df.corr()
 print(corr_mat)
 
@@ -65,3 +72,6 @@ sns.heatmap(corr_mat, annot=True)  # Requires matplotlib version <=3.1.0
 plt.tight_layout()
 plt.show()
 # plt.savefig("./fig/pearson-corr-heatmap.png")
+
+# Export the analysis DF to CSV for ease of usage
+analysis_df.to_csv("./norm-stat-analysis-data.csv", sep=",")
